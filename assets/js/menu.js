9df47ecd2f5ea4a7d18ee2df45e0d100c7939777
Vineet Dev <vineet.dev@vineet-latitude-e5450.jasperindia.local>
2016-12-14 17:44:53 +0530

    new menu made
    Signed-off-by: Vineet Dev <vineet.dev@vineet-latitude-e5450.jasperindia.local>

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,21 +2,185 @@ const {remote} = require('electron')
 const {Menu, MenuItem} = remote
 
 // Build our new menu
-const menu = new Menu()
-menu.append(new MenuItem({
-  label: 'Delete',
-  click: function() {
-    // Trigger an alert when menu item is clicked
-    alert('Deleted')
+// const menu = new Menu()
+// menu.append(new MenuItem({
+//   label: 'Delete',
+//   click: function() {
+//     // Trigger an alert when menu item is clicked
+//     alert('Deleted')
+//   }
+// }))
+// menu.append(new MenuItem({
+//   label: 'More Info...',
+//   click: function() {
+//     // Trigger an alert when menu item is clicked
+//     alert('Here is more information')
+//   }
+// }))
+
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        role: 'undo'
+      },
+      {
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'cut'
+      },
+      {
+        role: 'copy'
+      },
+      {
+        role: 'paste'
+      },
+      {
+        role: 'pasteandmatchstyle'
+      },
+      {
+        role: 'delete'
+      },
+      {
+        role: 'selectall'
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        role: 'reload'
+      },
+      {
+        role: 'toggledevtools'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'resetzoom'
+      },
+      {
+        role: 'zoomin'
+      },
+      {
+        role: 'zoomout'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'togglefullscreen'
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {
+        role: 'minimize'
+      },
+      {
+        role: 'close'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://www.shopo.in') }
+      }
+    ]
   }
-}))
-menu.append(new MenuItem({
-  label: 'More Info...',
-  click: function() {
-    // Trigger an alert when menu item is clicked
-    alert('Here is more information')
-  }
-}))
+]
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      {
+        role: 'about'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  })
+  // Edit menu.
+  template[1].submenu.push(
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Speech',
+      submenu: [
+        {
+          role: 'startspeaking'
+        },
+        {
+          role: 'stopspeaking'
+        }
+      ]
+    }
+  )
+  // Window menu.
+  template[3].submenu = [
+    {
+      label: 'Close',
+      accelerator: 'CmdOrCtrl+W',
+      role: 'close'
+    },
+    {
+      label: 'Minimize',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    },
+    {
+      label: 'Zoom',
+      role: 'zoom'
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Bring All to Front',
+      role: 'front'
+    }
+  ]
+}
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu)
 
 // Add the listener
 document.addEventListener('DOMContentLoaded', function () {
